6f71579348ce72f4956a6a3c3d134a8711a235fb
Sheikh Md Obaidullah <obaidullah2p17@gmail.com>
2023-02-14 19:11:32 +0600

    first 2

--- Week-08/Module_27/problem1.c
+++ Week-08/Module_27/problem1.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 
 struct Point{
+    public:
     int x;
     int y;
 };
 
+int manhattan(Point p1, Point p2)
+{
+    int res = abs(p1.x - p2.x) = abs(p1.y - p2.y);
+    return res;
+}
 int main()
 {
     struct Point p1 = { 1, 1 };
