71c8fc4b31f1f036efa52c815a283e098a8210dd
Sheikh Md Obaidullah <obaidullah2p17@gmail.com>
2023-02-14 16:39:24 +0600

    first

--- Week-08/Module_27/problem1.c
+++ Week-08/Module_27/problem1.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
+struct Point{
+    int x;
+    int y;
+};
+
 int main()
 {
-     
+    struct Point p1 = { 1, 1 };
+    struct Point p2 = { 4, 5 };
+
+    int ans = manhattan(p1, p2);
+    return 0;
 }
